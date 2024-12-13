7731b4d45116434a50f2db5a99609ec42b2ca5b3
maryajii <maryaji2003@gmail.com>
2024-12-13 05:31:23 +0000

    rico_recipe password for database

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "rico_recipes_app",
-  password: "rico",
+  password: "mary123",
   database: "rico_recipes",
 });
 
